3040fc480add0da3a03fc84e7e14b263f0cdaae9
Cauê Shimoda <cauefshimoda@gmail.com>
2025-01-10 11:47:58 -0300

    Added some documentation to tic_tac_oh.c and TTO_conditions.h

--- TTO_conditions.h
+++ TTO_conditions.h
@@ -3,11 +3,14 @@
 #include "TTO_structures.h"
 
 
+// Returns the board's winner or 0 if there's no winner
 int winner(BOARD board) {
 	int row, col, check = 1;
 	char ref;
+	
+	// Check all rows for a full sequence of X's or O's
 	for (row = 0; row < board.matrix; row++) {
-		ref = board.placed_piece[row][0];
+		ref = board.placed_piece[row][0]; // sets the reference as the piece on the first column of the current row
 		if (ref == 'X' || ref == 'O'){
 			for (col = 1; col < board.matrix; col++) {
 				if (board.placed_piece[row][col] != ref) break;
@@ -17,8 +20,10 @@ int winner(BOARD board) {
 			check = 1;
 		}	
 	}
+	
+	// Check all columns for a full sequence of X's or O's
 	for (col = 0; col < board.matrix; col++) {
-		ref = board.placed_piece[0][col];
+		ref = board.placed_piece[0][col]; // sets the reference as the piece on the first row of the current column
 		if (ref == 'X' || ref == 'O'){
 			for (row = 1; row < board.matrix; row++) {
 				if (board.placed_piece[row][col] != ref) break;
@@ -28,6 +33,8 @@ int winner(BOARD board) {
 			check = 1;
 		}	
 	}
+	
+	// Check first diagonal, setting the reference as the piece on the first row and first column and incrementing both in the loop
 	ref = board.placed_piece[0][0];
 	check = 1;
 	if (ref == 'X' || ref == 'O') {
@@ -39,6 +46,8 @@ int winner(BOARD board) {
 		}
 		if (check == board.matrix) return ref;
 	}
+	
+	// Check second diagonal, setting the reference as the piece on the first row and last column, incrementing the row and decreasing the column in the loop
 	check = 1;
 	col = board.matrix - 1;
 	ref = board.placed_piece[0][col];
@@ -51,18 +60,24 @@ int winner(BOARD board) {
 		}
 		if (check == board.matrix) return ref;	
 	}		
+	
 	return 0;
 }  
 
 
+// Returns 1 if the standard condition to completing a level is met and 0 if it's not
 int std_cond(BOARD board) {
 	if (winner(board) == board.target) return 1;
 	return 0;
 }
 
 
+// Returns 1 if the colored condition to completing a level is met and 0 if it's not
+// Colored condition: all positions that are set as green slots must have a target sprite on them,
+// all positions that are set as red slots must have a wrong sprite on them
 int color_cond(BOARD board) {
 	int i;
+	
 	for (i = 0; i < board.special_count; i++){
 		if (board.special[i].type == GREEN_SLOT) {
 			if (board.placed_piece[board.special[i].row][board.special[i].column] != board.target) return 0; 
@@ -71,12 +86,17 @@ int color_cond(BOARD board) {
 			if (board.placed_piece[board.special[i].row][board.special[i].column] != board.wrong) return 0; 
 		} 
 	}
+	
 	return 1;
 }
 
 
+// Return 1 if the summation condition to completing a level is met and 0 if it's not
+// Summation condition: all rows and columns must have their respective target sums met
+// Blank space = 0, O = 1, X = 2, Bomb timer = the current timer number
 int summation_cond(BOARD board) {
 	int row, col, sum, sum_index = 0;
+	
 	for (row = 0; row < board.matrix; row++) {
 		sum = 0;
 		for (col = 0; col < board.matrix; col++) {
@@ -87,6 +107,7 @@ int summation_cond(BOARD board) {
 		if (sum != board.conditions.sum[sum_index]) return 0;
 		sum_index ++;
 	}
+	
 	for (col = 0; col < board.matrix; col++) {
 		sum = 0;
 		for (row = 0; row < board.matrix; row++) {
@@ -97,27 +118,35 @@ int summation_cond(BOARD board) {
 		if (sum != board.conditions.sum[sum_index]) return 0;
 		sum_index ++;
 	}
+	
 	return 1;
 }
 
 
+// Returns 1 if all the necessary conditions for a level to be completed are met, and 0 if they're not
 int completed(BOARD board) {
+	
 	if (board.conditions.has_colored_slot) {
 		if (!color_cond(board)) return 0;	
 	}
+	
 	if (board.conditions.summation) {
 		if (!summation_cond(board)) return 0;
 	}
+	
+	// Special condition when there's no target, and the level must end as a draw to be completed (no free slots and no winner)
 	if (board.target == ' ') {
 		if (!board.free_slots) {
 			if (!winner(board)) return 1;
 		} 
 		return 0;
 	} 
+	
 	return std_cond(board);
 }
 
 
+// Condition to the secret final level
 int final_cond(int quadrants[N_QUADRANTS]) {
 	int i, j, check_green = 0, check_red = 0;
 	for (i = 0; i < N_QUADRANTS; i++) {
--- tic_tac_oh.c
+++ tic_tac_oh.c
@@ -5,58 +5,78 @@
 #include "TTO_conditions.h"
 
 
+enum options {EXIT_GAME = 0, NEW_GAME = 2};
+
+
 int main() {
 	int last_lvl, choice, state = 0, lvls[MAX_LVL];
-	last_lvl = load_save(lvls);
+	last_lvl = load_save(lvls); //from TTO_functions.h
 	printf("tic tac oh...\nby cau\210 shimoda\n\n");
-	choice = menu(last_lvl);
-	if (choice) {
-		if (choice == 2) {
+	choice = menu(last_lvl); //from TTO_functions.h
+	
+	if (choice != EXIT_GAME) {
+		if (choice == NEW_GAME) {
 			last_lvl = 1;
-			save(last_lvl);
+			save(last_lvl); //from TTO_functions.h
 		}
-		initialize_lvls_state(lvls, last_lvl);
-		BOARD board;
+		initialize_lvls_state(lvls, last_lvl); //from TTO_functions.h
+		BOARD board; //from TTO_structures.h
 		choice = last_lvl;
-		if (choice <= MAX_LVL) load_lvl("lvls.txt", choice, &board);
+		if (choice <= MAX_LVL) load_lvl("lvls.txt", choice, &board); //from TTO_functions.h
+		
+		// Main game loop: increments last_lvl when a level is completed until it surpasses MAX_LEVEL
 		while (last_lvl <= MAX_LVL) {
-			while (!completed(board)){
-				if (state > 1) {
-					choice = map(last_lvl, lvls, 1);
+			while (!completed(board)){ //from TTO_conditions.h
+				// Call map function when player chooses to see the level map. The argument 1 makes it call the map menu
+				if (state > 1) { // state > 1: see level map
+					choice = map(last_lvl, lvls, 1); //from TTO_functions.h
 					if (choice) {
-						load_lvl("lvls.txt", choice, &board);
+						load_lvl("lvls.txt", choice, &board); // Load picked level from level map
 					}
 					state = 0;
 				} 
-				else state = game_loop(&board, state, last_lvl);	
+				else state = game_loop(&board, state, last_lvl); //from TTO_functions.h	
 			}
+			
+			// Draw board in green after a level is completed 
+			// Changing the state to WON ensures the draw_board function will print it in green
+			// The argument 0 says it's not a transition drawing
 			board.state = WON;
-			draw_board(board, 0);
+			draw_board(board, 0); //from TTO_functions.h
 			printf("\n");
+			
+			// Increment last_lvl if the players was playing the last level
 			if (board.lvl == last_lvl) {
 				lvls[last_lvl - 1] = LVL_COMPLETED;
 				lvls[last_lvl] = LVL_PLAYING;
 				last_lvl++;
 				save(last_lvl);
 				choice = last_lvl;
-			} 
+			}
+			// Go back to map if the players was paying an already completed level 
 			else {
 				choice = map(last_lvl, lvls, 1);
 				if (!choice) choice = last_lvl;
 			}
+			
 			load_lvl("lvls.txt", choice, &board);
 			state = 0;	
 		}
+		
 		int quadrants[N_QUADRANTS], i;
 		for (i = 0; i < N_QUADRANTS; i++) {
 			quadrants[i] = LVL_COMPLETED;
 		}
+		
+		// When all the levels are completed, there's still a secret one to solve
 		while (1) {
 			state = 0;
 			choice = map(last_lvl, lvls, 1);
 			if (!choice) exit(0);
 			load_lvl("lvls.txt", choice, &board);
-			while (!winner(board) && board.free_slots > 0){
+			
+			// Game loop for picked level
+			while (!winner(board) && board.free_slots > 0){ //winner(board) from TTO_conditions.h
 				if (state > 1) {
 					choice = map(last_lvl, lvls, 1);
 					if (choice) {
@@ -66,18 +86,22 @@ int main() {
 				} 
 				else state = game_loop(&board, state, last_lvl);	
 			}
+			
+			// Update level map quadrant with the appropriate state (LVL_COMPLETED or LVL_RED)
 			if (completed(board)) {
 				board.state = WON;
 				draw_board(board, 0);
-				if (lvls[choice - 1] != LVL_COMPLETED) update_lvls(lvls, quadrants, choice, LVL_COMPLETED);
+				if (lvls[choice - 1] != LVL_COMPLETED) update_lvls(lvls, quadrants, choice, LVL_COMPLETED); //from TTO_functions.h 
 			}
 			else {
 				board.state = LOST;
 				draw_board(board, 0);
 				if (lvls[choice - 1] != LVL_RED) update_lvls(lvls, quadrants, choice, LVL_RED);
 			}
-			if (final_cond(quadrants)) break;
+			if (final_cond(quadrants)) break; //from TTO_conditions.h
 		}
+		
+		// Draw the map one last time, with argument 0 so it doesn't call the map menu
 		map(last_lvl, lvls, 0);
 		printf(ANSI_BLUE);
 		printf("\nTHE END\n");
